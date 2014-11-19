765d2f76d6eaa9e5c3b8d186ed0fc539f1e468b8
mtbvang <mtb.vang@gmail.com>
2014-11-19 14:12:18 +0100

    Refactored vagrant class to accept array of plugins and versions to install.

--- manifests/ubuntu/vagrant.pp
+++ manifests/ubuntu/vagrant.pp
@@ -2,13 +2,14 @@
 # Install vagrant and plugins.
 #
 class common::ubuntu::vagrant (
-  $downloadUrl              = "https://dl.bintray.com/mitchellh/vagrant/vagrant_1.6.3_x86_64.deb",
-  $user                     = 'dev',
-  $userHome                 = '/home/dev',
-  $hostPluginVersion        = '2.1.5',
-  $vbguestPluginVersion     = '0.10.0',
-  $hostsupdatePluginVersion = '0.0.11',
-  $cachierPluginVersion     = '1.1.0') {
+  $downloadUrl = "https://dl.bintray.com/mitchellh/vagrant/vagrant_1.6.3_x86_64.deb",
+  $user        = 'dev',
+  $userHome    = '/home/dev',
+  $plugins     = [
+    'vagrant-hosts,2.1.5',
+    'vagrant-vbguests,0.10.0',
+    'vagrant-hostsupdater,0.0.11',
+    'vagrant-cashier,1.1.0']) {
   ::wget::fetch { "fetchVagrant":
     before      => Package['vagrant'],
     source      => $downloadUrl,
@@ -27,44 +28,9 @@ class common::ubuntu::vagrant (
     source   => "/tmp/vagrant.deb",
   }
 
-  exec { 'vagrant_hosts_plugin':
-    environment => "HOME=${userHome}",
-    command     => "/usr/bin/vagrant plugin install vagrant-hosts --plugin-version ${hostPluginVersion}",
-    require     => [Package['vagrant']],
-    before      => File["${userHome}/.vagrant.d"],
-    logoutput   => on_failure,
-    unless      => "vagrant plugin list | grep 'vagrant-hosts (${hostPluginVersion})'",
-    user        => $user,
-  }
-
-  exec { 'vagrant_vbguest_plugin':
-    environment => "HOME=${userHome}",
-    command     => "/usr/bin/vagrant plugin install vagrant-vbguest --plugin-version ${vbguestPluginVersion}",
-    require     => [Package['vagrant']],
-    before      => File["${userHome}/.vagrant.d"],
-    logoutput   => on_failure,
-    unless      => "vagrant plugin list | grep 'vagrant-vbguest (${vbguestPluginVersion})'",
-    user        => $user,
-  }
-
-  exec { 'vagrant_hostsupdater_plugin':
-    environment => "HOME=${userHome}",
-    command     => "/usr/bin/vagrant plugin install vagrant-hostsupdater --plugin-version ${hostsupdatePluginVersion}",
-    require     => [Package['vagrant']],
-    before      => File["${userHome}/.vagrant.d"],
-    logoutput   => on_failure,
-    unless      => "vagrant plugin list | grep 'vagrant-hostsupdater (${hostsupdatePluginVersion})'",
-    user        => $user,
-  }
-
-  exec { 'vagrant_cachier_plugin':
-    environment => "HOME=${userHome}",
-    command     => "/usr/bin/vagrant plugin install vagrant-cachier --plugin-version ${cachierPluginVersion}",
-    require     => [Package['vagrant']],
-    before      => File["${userHome}/.vagrant.d"],
-    logoutput   => on_failure,
-    unless      => "vagrant plugin list | grep 'vagrant-cachier (${cachierPluginVersion})'",
-    user        => $user,
+  common::ubuntu::vagrant::plugin { $plugins:
+    before  => File["${userHome}/.vagrant.d"],
+    require => [Package['vagrant']],
   }
 
   file { "${userHome}/.vagrant.d":
@@ -72,8 +38,20 @@ class common::ubuntu::vagrant (
     ensure  => 'directory',
     owner   => $user,
     group   => $user,
-    # mode    => '0755',
     recurse => true,
   }
+}
 
+define common::ubuntu::vagrant::plugin ($user = 'dev', $userHome = '/home/dev',) {
+  $plugin = split($name, ',')
+
+  exec { 'vagrant_cachier_plugin':
+    environment => "HOME=${userHome}",
+    command     => "/usr/bin/vagrant plugin install ${plugin[0]} --plugin-version ${plugin[1]}",
+    require     => [Package['vagrant']],
+    before      => File["${userHome}/.vagrant.d"],
+    logoutput   => on_failure,
+    unless      => "vagrant plugin list | grep '${plugin[0]} (${plugin[1]})'",
+    user        => $user,
+  }
 }

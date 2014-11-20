feb4ac61e6bd99de7caf0ff7b8f370f4e95aa827
mtbvang <mtb.vang@gmail.com>
2014-11-20 18:07:27 +0100

    Added virtualbox class to install from Oracle repos because 4.3.10 has a major bug that doesn't allow nesting.

---
+++ manifests/ubuntu/virtualbox.pp
@@ -0,0 +1,16 @@
+class common::ubuntu::virtualbox ($packageName = "virtualbox-4.3") {
+  apt::source { virtualbox:
+    location    => 'http://download.virtualbox.org/virtualbox/debian',
+    release     => 'trusty',
+    repos       => 'contrib non-free',
+    key         => '98AB5139',
+    key_source  => "http://download.virtualbox.org/virtualbox/debian/oracle_vbox.asc",
+    include_src => false,
+  }
+
+  package { virtualbox:
+    name    => 'virtualbox-4.3',
+    ensure  => present,
+    require => Apt::Source['virtualbox']
+  }
+}
